42a577e6818dadaebfdd9c3053482f01cb790515
unknown <gwenpena21@gmail.com>
2022-06-30 22:16:45 +0800

    added some changes on GridPics

--- src/components/pages/GridsPics.js
+++ src/components/pages/GridsPics.js
@@ -5,10 +5,10 @@ function GridsPics() {
   return (
     <div className="grid-pics-container">
 
-            <img src="/images/team1_large.jpg" alt="Team" />
-            <img src="/images/team1_small.jpg"  alt="Team" />
-            <img src="/images/team2_small.jpg"  alt="Team" />
-            <img src="/images/team2_large.jpg" alt="Team" />
+            <img src="./images/team1_large.jpg" alt="Team" />
+            <img src="./images/team1_small.jpg"  alt="Team" />
+            <img src="./images/team2_small.jpg"  alt="Team" />
+            <img src="./images/team2_large.jpg" alt="Team" />
     
 
     </div>
